9dd6083675c5a12549b6d2391feb6dc4448e2b7f
FaizulOsman <faizulosman@gmail.com>
2022-09-28 23:15:22 +0100

    font awesome installed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { faCoffee, faDumbbell, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function App() {
   return (
     <div className="App">
       <h1>Hey! This is 8th Assignment</h1>
       <button className="btn btn-primary">Button</button>
+      <FontAwesomeIcon icon={faCoffee}></FontAwesomeIcon>
+      <FontAwesomeIcon icon={faDumbbell}></FontAwesomeIcon>
     </div>
   );
 }
